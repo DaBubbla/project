5481e9e4c13ed5eab83d062b2d11ddbeb4a63b44
Edward Johnston <>
2019-01-04 17:14:07 +0000

    Writing big if-else

--- application.py
+++ application.py
@@ -1,17 +1,15 @@
 import os
 import json
 import requests
-import sqlite3 # SQL might handle as well
-import csv
+# import csv
 
 from cs50 import SQL
-from flask import Flask, flash, render_template, request, redirect, session, url_for # For UI / UX
-from flask_session import Session # For UI / UX
+from flask import Flask, flash, render_template, request, session, url_for
+from flask_session import Session
 
 from tempfile import mkdtemp
-from helpers import login_required, lookup, apology, usd
+from helpers import apology, pyson, usd# login_required, lookup, # Necessary for later application
 
-from werkzeug.security import check_password_hash, generate_password_hash # For login info
 
 
 # Configure application
@@ -42,120 +40,138 @@ db = SQL("sqlite:///project.db")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # take csv as input
-    try:
-        conn = sqlite3.connect("project.db")
-    except:
-        return apology("Error with db connect")
-    # Appoint cursor to exec queries against db
-    cur = conn.cursor()
-    # Fetch 1st 100 rows from watch table
-    a = cur.execute("SELECT * FROM watch ORDER BY date DESC LIMIT 0, 99")
+    if request.method == "GET":
 
-    # Fetchall will grab all results of a query
-    b = a.fetchall()
+        # pyson()  Gives PYSON data.
 
-    # print(type(b)) # Indicates list
+        q = pyson()
+        p = []
+        # For analysis block#####
+        days = 9
+        for i in range(days):
+            p.append(
+                q[i]
+                )
+        #########################
 
-    c = []
-    for i in b:
-        # print(i[0]) Got-eem!
-        c.append({
-            'date': i[0],
-            'open': i[1],
-            'close': i[4],
-            'x': i[3]
-            })
-        # print(c) Successfully prints array of objects
+        # Evaluate trends in data
+        Min = 0
+        Max = 9999.99
+        BullFlip = 0
+        BearFlip = 0
+        CountDown = 0
+        count = 0
+        Reference = 0
+        Support = 0
 
-    for w in range(len(c)):
-        Open = c[w]['open'] # 0.5
-        Close= c[w]['close']# 0.5
-        u_d  = c[w]["x"]# 'down'
-        if Open <= Close:
-            c[w]['x']='upper'
+        switch = 1
+
+        for i in range(len(p)):
+            if switch == 0:
+                if p[i]['low'] < Min:
+                    Min = p[i]['low']
+                if p[i]['high'] > Max:
+                    Max = p[i]['high']
+            else:
+                # Initialize min, max, last close on 1st iteration
+                Min = p[i]['low']
+                Max = p[i]['high']
+                Last = p[i]['close']
+                switch = 0 # Turn this off for rest of loop
+            print(p[i])
+
+        if BearFlip or BullFlip:
+            if BearFlip and p[i]['close'] < Reference:
+                CountDown = CountDown + 1
+                if CountDown == 9:
+                    """BuySignal"""
+                    # Reference
+                    low1 = p[i-8]['low']
+                    low2 = p[i-7]['low']
+                    low3 = p[i-6]['low']
+                    low4 = p[i-5]['low']
+                    low5 = p[i-4]['low']
+                    low6 = p[i-3]['low']
+                    low7 = p[i-2]['low']
+                    low8 = p[i-1]['low']
+                    low9 = p[i]
+                    if low6 < low7:
+                        # discuss logic
+                        low1 = low6
+                    else:
+                        low1 = low7
+                        ###############
+                    if low8 < low9:
+                        # discuss logic
+                        low2 = low8
+                    else:
+                        low2 = low9
+                        ###############
+                    if low1 > low2:
+                        print('Doing things here')
+                        # Strong_sell
+                else:
+                    print("Or doing stuff there")
+                    # Strong_sell
+                CountDown = 0
+                BullFlip = 0
+                BearFlip = 0
+                Reference = 0
+            else:
+                CountDown = 0
+                BullFlip = 0
+                BearFlip = 0
+                print("BUST")
+                Reference = 0
         else:
-            c[w]['x']='down'
-        print(c[w])
+            # Reference
+            c1 = p[i-4]['close']
+            c4 = p[i-1]['close']
+
+            if i > 3 and c4 > c1 and p[i]['close'] < c1:
+                # BearFlip
+                print("BearFlip: ", p[i]['close'])
+                BearFlip=1
+                Reference = c4
+                CountDown = CountDown + 1
+
+            print('oh lawdie me, this a long if')
+
+        return render_template("watch.html")
 
 
+        # trend = []
 
+        # for i in range(len(p)):
+        #     # ID a trend
+        #     if p[i]['u_d'] == "upper" and count <= 9:
+        #         trend = []
+        #         trend.append({
+        #             'date': p[i]['date'],
+        #             'open': p[i]['open'],
+        #             'high': p[i],
+        #             'low' : p[i],
+        #             'close': p[i],
+        #             'u_d': p[i]
+        #         })
+        #         count = count + 1
 
-
-
-
-    return render_template("watch.html")
-
-
-
-    # x = {}
-    #     # 'date': thingy[0],
-    #     # 'open': thingy[1],
-    #     # 'high': thingy[2],
-    #     # 'low':  thingy[3],
-    #     # 'close':thingy[4],
-    #     # 'adj_close':thingy[5],
-    #     # 'volume': thingy[6],
-    #     # 'updown': thingy[7]
-
-    # i=0
-    # for i in range(len(thingy)):
-    #     x.append(thingy[i])
-    #     i = i + 1
-
-    # print(x) #returns csv list
-
-
-
-
-
-    # for row in rows:
-    #     Open = db.execute("SELECT 'open' FROM watch")
-    #     Close = db.execute("SELECT 'close' FROM watch")
-    #     updown = db.execute("SELECT 'updown' FROM watch")
-
-    #     if Open < Close:
-    #         cur.executemany("UPDATE watch SET updown=0 WHERE rowid=?")
-    #     else:
-    #         cur.executemany("UPDATE 'watch' SET 'updown'=0 WHERE rowid=$rowid")
-
-
-
-
-
-
-        # for col in rows:
-        #     count = 0
-        #     Open = col[1]
-        #     High = col[2]
-        #     Low = col[3]
-        #     Close = col[4]
-        #     updown = col[7]
-
-        #     if col[1] <= col[4]:
-        #       db.execute("UPDATE watch SET 'updown'=1 WHERE rowid=rows[i]")# UPPER
-        #       countDown = countDown + 1
-
-
+        #     elif p[i]['u_d'] == "downer" and count <= 9:
+        #         trend = []
+        #         trend.append({
+        #             'date': i[0],
+        #             'open': i[1],
+        #             'high': i[2],
+        #             'low' : i[3],
+        #             'close': i[4],
+        #             'u_d': i[7]
+        #         })
+        #         count = count + 1
         #     else:
-        #         db.execute("UPDATE watch SET 'updown'=0")# DOWNER
-        #         countDown = 0
+        #         count = 0
+        #         trend = []
+        #     print(trend)
 
-        #     count = count + 1
-
-            # Stuff into database
-            # col = db.execute("INSERT INTO table poopie (date, open, high, low, close, updown) VALUES ( date=col[0], open=col[1], high=col[2],low=col[3], close=col[4], updown=col[7] ", )
-
-
-
-
-        # BullFlip = 0
-        # BearFlip = 0
-        # CountDown = 0
-        # Reference = 0
-        # Support = 0
-        # min = 999.00
-        # max = 0
 
     # if bearFlip or bullFlip:
     #     if bearFlip and close < reference:
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import requests
 import urllib.parse
+import sqlite3
 
 from flask import redirect, render_template, request, session
 from functools import wraps
@@ -19,41 +20,82 @@ def apology(message, code=400):
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
-
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
-
-
-def lookup(symbol):
-    """Look up quote for symbol."""
-
-    # Contact API
+def pyson():
+    """Convert SQLite data to mutable data"""
+    # Take data in reverse order + sectioned (100 days)
     try:
-        response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
-        response.raise_for_status()
-    except requests.RequestException:
+        conn = sqlite3.connect("project.db")
+    except:
+        print("Error with db connect")
         return None
+    # Appoint cursor to exec queries against db
+    cur = conn.cursor()
+    # Fetch 1st 100 rows from watch table
+    a = cur.execute("SELECT * FROM watch ORDER BY date DESC LIMIT 0, 99")
 
-    # Parse response
-    try:
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
-        }
-    except (KeyError, TypeError, ValueError):
-        return None
+    # Fetchall will grab all results of a query
+    b = a.fetchall()
+    c = []
+
+
+    for i in b:
+        # print(i[0]) Got-eem!
+        c.append({
+            'date': i[0],
+            'open': i[1],
+            'high': i[2],
+            'low' : i[3],
+            'close': i[4],
+            'u_d': i[7]
+            })
+    # Indicates if day trend is going up or down
+    for i in range(len(c)):
+        Open = c[i]['open'] # 0.5
+        High = c[i]['high']
+        Low = c[i]['low']
+        Close= c[i]['close']# 0.5
+        u_d  = c[i]["u_d"]# 'down'
+        if Open <= Close:
+            c[i]['u_d']='upper'
+        else:
+            c[i]['u_d']='down'
+        # print(c)
+    return c
+
+# def login_required(f):
+#     """
+#     Decorate routes to require login.
+
+#     http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+#     """
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         if session.get("user_id") is None:
+#             return redirect("/login")
+#         return f(*args, **kwargs)
+#     return decorated_function
+
+
+# def lookup(symbol):
+#     """Look up quote for symbol."""
+
+#     # Contact API
+#     try:
+#         response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
+#         response.raise_for_status()
+#     except requests.RequestException:
+#         return None
+
+#     # Parse response
+#     try:
+#         quote = response.json()
+#         return {
+#             "name": quote["companyName"],
+#             "price": float(quote["latestPrice"]),
+#             "symbol": quote["symbol"]
+#         }
+#     except (KeyError, TypeError, ValueError):
+#         return None
 
 
 
